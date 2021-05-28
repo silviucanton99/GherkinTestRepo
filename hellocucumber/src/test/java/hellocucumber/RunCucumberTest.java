f5f2ab4113146d631379c73affdaba2238c100ed
Silviu-Constantin Anton <silviu-constantin.anton@microfocus.com>
2021-05-28 14:22:58 +0300

    changed to html report

--- hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
+++ hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "junit:target/cucumber-reports/Cucumber.xml"})
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports/Cucumber.html"})
 public class RunCucumberTest {
 
 }
