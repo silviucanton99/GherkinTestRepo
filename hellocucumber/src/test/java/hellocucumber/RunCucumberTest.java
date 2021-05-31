4c502a29aec652ee3988c7e74c3ceae74b04a41f
Silviu-Constantin Anton <silviu-constantin.anton@microfocus.com>
2021-05-31 11:29:51 +0300

    Added octane cucumber plugin

--- hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
+++ hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
@@ -1,11 +1,11 @@
 package hellocucumber;
 
+import com.hpe.alm.octane.OctaneGherkinFormatter;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"})
+@CucumberOptions(plugin="com.hpe.alm.octane.OctaneGherkinFormatter:target/cucumber-reports/Cucumber.xml")
 public class RunCucumberTest {
-
 }
