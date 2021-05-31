a1286ccc7d1a73c9809d4347b867686538ca30fe
Silviu-Constantin Anton <silviu-constantin.anton@microfocus.com>
2021-05-31 12:33:59 +0300

    Changed test output to json

--- hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
+++ hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin="json:target/cucumber-reports/Cucumber.json")
+@CucumberOptions(plugin="json:results/cucumber/Cucumber.json")
 public class RunCucumberTest {
 }
